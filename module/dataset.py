f368cd2076ea0bd95cdf4226bf425a5659c75eef
imsuviiix <gydbs0925@naver.com>
2024-08-12 21:25:13 +0900

    modulization and attach kpfbert dir

---
+++ module/dataset.py
@@ -0,0 +1,30 @@
+import torch
+from torch.utils.data import Dataset
+
+class TextDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer, max_len):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = self.labels[idx]
+        encoding = self.tokenizer(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt',
+        )
+        return {
+            'text': text,
+            'input_ids': encoding['input_ids'].squeeze(0),
+            'attention_mask': encoding['attention_mask'].squeeze(0),
+            'labels': torch.tensor(label, dtype=torch.long)
+        }
---
+++ module/model_infer.py
@@ -0,0 +1,38 @@
+#단일 문장 혹은 문단을 추론하고 싶으면 아래 코드 이용
+
+import torch
+from transformers import BertForSequenceClassification, BertTokenizer
+
+model_name_or_path = "bubble_free_BERT"
+tokenizer_name_or_path = "bubble_free_tokenizer"
+
+model = BertForSequenceClassification.from_pretrained(model_name_or_path)
+tokenizer = BertTokenizer.from_pretrained(tokenizer_name_or_path)
+
+model.eval()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+sentence = "27일 TV조선은 ‘이것 밖에 방법이 없어’라는 내용의 유서 일부를 공개했다."
+
+inputs = tokenizer(sentence, return_tensors="pt", max_length=128, truncation=True, padding="max_length")
+
+inputs = {key: value.to(device) for key, value in inputs.items()}
+
+with torch.no_grad():
+    outputs = model(**inputs)
+    logits = outputs.logits
+
+    softmax = nn.Softmax(dim=1)  # dim=1은 클래스 차원에 대해 softmax를 적용
+    preds = softmax(logits)
+
+
+preds_rounded = preds.cpu().numpy().round(4)
+
+predicted_class_id = torch.argmax(preds, dim=1).item()
+predicted_class = model.config.id2label[predicted_class_id] if model.config.id2label else predicted_class_id
+
+print(f"Predicted class: {predicted_class}")
+print("Softmax probabilities:", preds_rounded)
+print("Logits:", logits)
