3e2854acb387753db1697306ca7c30b3f185f851
imsuviiix <gydbs0925@naver.com>
2024-08-18 22:34:39 +0900

    change some fe and api:

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,13 +1,28 @@
-// fe/App.js
+// src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
+import HomePage from './pages/HomePage';
+import MyPage from './pages/MyPage';
 import './App.css';
 
 const App = () => {
     return (
-        <div className="App">
-            <SearchBar />
-        </div>
+        <Router>
+            <div className="App">
+                <nav>
+                    <ul>
+                        <li><Link to="/">Main Page</Link></li>
+                        <li><Link to="/mypage">My Page</Link></li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/mypage" element={<MyPage />} />
+                </Routes>
+                <SearchBar />
+            </div>
+        </Router>
     );
 };
 
---
+++ fe/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+// src/pages/HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default HomePage;
